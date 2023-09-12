ebc0deca137c0bb62777f38bab3db9afc9148aa6
Luis Mansilla <l_mansilla92@yahoo.com>
2023-09-12 14:58:05 -0500

    Modified api url to call for weather based on text area value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,36 @@
 const APIKey = "47918640400e32d5637e553627ff14e4";
 var weatherToday = document.querySelector(".today");
 var searchBtn = document.querySelector(".search");
+const todayContainer = document.querySelector(".today");
+const weekContainer = document.querySelector(".week");
+let cityNameInput = document.querySelector(".city-name");
 
 
 // Base URL
 // http://api.openweathermap.org/data/2.5/weather
 
+// Hides weather information sections
+todayContainer.style.display = "none";
+weekContainer.style.display = "none";
+
+
 searchBtn.addEventListener ("click", function(event) {
     // Prevents button from submitting so the code can run and call the getApi function
     event.preventDefault();
+    // Calls function that gets API request
     getApi();
     console.log("button clicked");
+    // Displays sections with weather information
+    if (todayContainer.style.display === "none") {
+        todayContainer.style.display = "block";
+    } else {
+        return;
+    }
 });
 
 function getApi() {
-    var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=' + APIKey;
+    cityNameValue = cityNameInput.value;
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityNameValue + "&appid=" + APIKey;
     // fetch request to open weather map
     fetch(requestUrl)
       .then(function (response) {
@@ -26,20 +42,20 @@ function getApi() {
 
           // Grabs elements to display info from API
           debugger;
-          var cityName = document.querySelector(".city-name");
-          cityName.textContent = data.city.name;
+          var cityName = document.querySelector(".city-h2");
+          cityName.textContent = data.name;
 
           var tempEl = document.querySelector(".temp-today");
-          tempEl.textContent = "Temp: " + data.list[0].main.temp;
+          tempEl.textContent = "Temp: " + data.main.temp;
 
           var windEl = document.querySelector(".wind-today");
-          windEl.textContent = "Wind: " + data.list[0].wind.speed;
+          windEl.textContent = "Wind: " + data.wind.speed;
 
           var humidityEl = document.querySelector(".humidity-today");
-          humidityEl.textContent = "Humidity: " + data.list[0].main.humidity;
+          humidityEl.textContent = "Humidity: " + data.main.humidity;
         }
       );
-  }
+  };
 
 //   var cardBody = document.createElement('div');
 //   cardBody.classList.add("border p-2");
