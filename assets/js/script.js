eef4782a1f7e85f5357e41f0566b503f740d9ec5
Luis Mansilla <l_mansilla92@yahoo.com>
2023-09-13 20:21:46 -0500

    added current date to current weather section

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ searchBtn.addEventListener ("click", function(event) {
 
 // Current Weather Fetch
 function getApi() {
-    cityNameValue = cityNameInput.value;
+    cityNameValue = cityNameInput.value.trim();
     var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityNameValue + "&units=imperial&appid=" + APIKey;
     // fetch request to open weather map
     fetch(requestUrl)
@@ -39,13 +39,20 @@ function getApi() {
         })
         .then(function (data) {
             console.log(data)
-
+            // Gets current date
+            var date = new Date().toLocaleDateString(); // TODO: need to find date 
             // Grabs elements to display info from API
             var cityName = document.querySelector(".city-h2");
-            cityName.textContent = data.name;
+            // Sets city name and concatinates with the current date
+            cityName.textContent = data.name + ' (' + date + ')';
 
-            var date = document.querySelector(".date-today");
-            date.textContent = data.name; // TODO: need to find date 
+            var todayImgEl = document.querySelector(".today-img")
+            var imgCode = data.weather[0].icon;
+            console.log(imgCode);
+            imgUrl = 'https://openweathermap.org/img/wn/' + imgCode + '@2x.png';
+            todayImgEl.src = imgUrl;
+            // var date = document.querySelector(".date-today");
+    
 
             var tempEl = document.querySelector(".temp-today");
             tempEl.textContent = "Temp: " + data.main.temp;
@@ -90,3 +97,35 @@ function getApi() {
             };
         });
 };
+
+// How to get icon URL
+// For code 500 - light rain icon = "10d". See below a full list of codes
+// URL is https://openweathermap.org/img/wn/10d@2x.png
+
+
+
+            // renderForecastCard();
+
+            // function renderForecastCard(forecast) {
+            //     // variables for data from api
+            //     var iconUrl = `https://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+            //     var iconDescription = forecast.weather[0].description;
+            //     var tempF = forecast.main.temp;
+            //     var humidity = forecast.main.humidity;
+            //     var windMph = forecast.wind.speed;
+               
+            //     // Create elements for a card
+            //     var col = document.createElement('div');
+               
+            //    // Append the data to the card
+            //     col.append(card);
+            //     card.append(cardBody);
+            //     cardBody.append(cardTitle, weatherrIcon, tempEl, windEl, humidityEl);
+               
+            //    // Set the attributes
+            //     col.setAttribute('class', 'col-md');
+               
+            //     // Add content to elements
+
+               
+            //     forecastContainer.append(col);
